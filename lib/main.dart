c24eff9d8b66a3dbd85596b71fbabddae09f391d
hasan <hassun.h98@gmail.com>
2024-06-27 23:22:18 +0300

    for test

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Doctor',
       theme: ThemeData(
+        
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
