35eab90bf99775eeaf080de9d6e88cc7f0750893
hasan <hassun.h98@gmail.com>
2024-06-28 13:19:33 +0300

    Merge changes from development to onBoarding while keeping onBoarding files unchanged

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,17 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:doctor/core/routing/app_router.dart';
-import 'package:doctor/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'package:doctor/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(DocApp(
-      appRouter: AppRouter(),
-    ));
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
