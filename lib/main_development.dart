b320739b0ae133055109e10f7eabf05c38c7b4bc
hasan <hassun.h98@gmail.com>
2024-07-15 10:39:16 +0300

    .

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -7,6 +7,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
+  //
   //To fix texts being hidden bug in flutter_ScreenUtil in release mode
   await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
